d1f082b5f45d793d7b8a90986c21f688839b73d9
Jiapan_Yu <2541771811@qq.com>
2020-07-14 18:00:11 +0800

    界面添加高德地图

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert("welcome page")
+import AMapLoader from '@amap/amap-jsapi-loader'
+
+AMapLoader.load({
+  "key": "cce9058e8e767e6dc4f66e8309bd4e12",
+  "version": "2.0",
+  "plugins": ['AMap.Scale'],
+}).then((AMap) => {
+  let map = new AMap.Map('container', {
+    zoom: 14,
+    center: [113.237192, 22.959006],
+    zooms: [13, 20],
+  })
+
+  map.addControl(new AMap.Scale())
+}).catch(e => {
+  console.log(e)
+})
